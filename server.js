139e6941e1e386aca9481defda053cbabf2f87a5
boby87 <fokoutghislain@gmail.com>
2022-05-29 23:10:58 +0100

    all

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/dist/appachatfront')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/appachatfront/index.html'));
+var http = require('http');
+var enforce = require('express-sslify');
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
 });
-app.listen(process.env.PORT || 8080);
+// Serve only the static files form the dist directory
+// https://medium.com/@ryanchenkie_40935/angular-cli-deployment-host-your-angular-2-app-on-heroku-3f266f13f352
+app.use(express.static('./dist/appachatfront'));
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/appachatfront'}),
+);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 4200, () => {
+  console.log(process.env.PORT);
+});
