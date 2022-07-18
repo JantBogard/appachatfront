763d0115c81b81576028995f380f496833e65f04
JantBogard <joancenkonga@gmail.com>
2022-07-18 23:46:43 +0100

    Fixed bug

--- src/app/service/Adresse.ts
+++ src/app/service/Adresse.ts
@@ -1,4 +1,4 @@
 export class Adresse{
-  static host:string="http://localhost:8080/";
-  // static host: string = "https://appachat.herokuapp.com/";
+  // static host:string="http://localhost:8080/";
+  static host: string = "https://appachat.herokuapp.com/";
 }
