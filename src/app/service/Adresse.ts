cf1b1711c985fa1b2b9e00b92124a63bb74768b4
boby87 <fokoutghislain@gmail.com>
2023-05-09 01:02:54 +0400

    nouvelle modif

--- src/app/service/Adresse.ts
+++ src/app/service/Adresse.ts
@@ -1,4 +1,4 @@
 export class Adresse{
-  static host:string="http://localhost:8082/";
-  // static host: string = "https://apachat-backend.herokuapp.com/";
+  //static host:string="http://localhost:8082/";
+   static host: string = "http://31.207.35.205:8082/";
 }
