652602d61bf73a3bbd2bb2ec0ca3187e2317510c
boby87 <48254604+boby87@users.noreply.github.com>
2023-05-14 20:30:56 +0400

    Update Adresse.ts

--- src/app/service/Adresse.ts
+++ src/app/service/Adresse.ts
@@ -1,4 +1,4 @@
 export class Adresse{
   //static host:string="http://localhost:8082/";
-  static host: string = "https://adzap-cs.com:8082/";
+  static host: string = "http://adzap-cs.com:8082/";
 }
